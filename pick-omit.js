50a174927d9cb900a956fb23c6b5f585e4d41d72
Arnaudboy <r.boyer895@laposte.net>
2021-05-26 11:30:25 +0200

    push the project

--- pick-omit.js
+++ pick-omit.js
@@ -1,5 +1,20 @@
 function pick(obj, keys) {
     let res = {}
-    Object.keys(obj).forEach(key => {if(keys.includes(key)){res[key] = obj[key]}})
+    if(Array.isArray(keys)) {
+        keys.forEach(key => {if(Object.keys(obj).includes(key)){res[key] = obj[key]}})
+    }
+    else if (Object.keys(obj).includes(keys)){
+        res[keys] = obj[keys]
+    }
+    return res
+}
+
+function omit(obj, keys) {
+    let res = {}
+    Object.keys(obj).forEach(key => {
+        if(!keys.includes(key)){
+            res[key] = obj[key]
+        }})
+    
     return res
 }
