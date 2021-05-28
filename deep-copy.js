4b174a7c196d734eda184ac2c7191dc994c896c1
Arnaudboy <r.boyer895@laposte.net>
2021-05-28 15:38:03 +0200

    push the project

--- deep-copy.js
+++ deep-copy.js
@@ -1,5 +1,22 @@
 function deepCopy(obj){
-    let copy = obj
-    console.log(copy === obj)
-    return copy
+    console.log(obj)
+    if(Array.isArray(obj)){return copyArray(obj)}
+    else {return copyObject(obj)}
+}
+
+const copyArray = input => {
+    let arr = []
+    for(let i = 0; i < input.length; i++){
+        if(Array.isArray(input[i])){copyArray(input[i])}
+        arr[i] = input[i]
+    }
+    console.log(arr)
+    return arr
+}
+const copyObject = input => {
+    let res = {}
+    for(const [key, value] of Object.entries(input)){
+        res[key] = value
+    }
+    return res
 }
