8d7847ec20c8afb6df1b1ffe828db36587ee2968
Arnaudboy <r.boyer895@laposte.net>
2021-05-30 04:20:39 +0200

    push the project

--- neuron.js
+++ neuron.js
@@ -18,32 +18,49 @@ function neuron(dataset){
     if(dataset.length===0){return {}}
     let res = {}
     let data = []
-    dataset.forEach(element => data.push(element.split('-')))
+    dataset.forEach(element => {
+        let tmp = element.split(/-/)
+        let first = tmp.shift()
+        data.push([first, tmp.join('-')])
+    }) 
     data = data.flat()
     let arr = []
     data.forEach(index => arr.push(String(index.split(': ')).trim()))
-    //console.log(arr)
     arr.forEach((element, index) => {
-        element = element.split(',');
-        let key = element[0].toLowerCase()
+        element = element.split(',')
+    let key = element[0].toLowerCase()
         if(res[key]=== undefined && index%2===0){
-            let key2 = element[1].replace(/\s/g, '_').replace('!', '')
+            let key2 = element[1].replace(/\s/g, '_').replace('!', '').replace('?', '')
+            //key2 = key2.toLowerCase()
+            //console.log(key2)
             let obj = {}
             let obj2 = {}
             obj2[key.substring(0, key.length - 1)] = element[1]
-            obj2[arr[index+1].toLowerCase().split(",")[0]+'s'] = [arr[index+1].split(",")[1]]
-            obj[key2] = obj2
+            let arr2 = arr[index+1].split(",")
+            let eleminate = arr2.shift()
+            obj2[arr[index+1].toLowerCase().split(",")[0]+'s'] = [arr2.join(',')]
+            obj[key2.toLowerCase()] = obj2
             res[key] = obj
         }
         else if (index%2===1){
-            key = arr[index-1].toLowerCase().split(',')[0]
-            let key2 = arr[index-1].toLowerCase().split(',')[1].replace(/\s/g, '_').replace('!', '')
+            let key1 = arr[index-1].toLowerCase().split(',')[0]
+            let key2 = arr[index-1].toLowerCase().split(',')[1].replace(/\s/g, '_').replace('!', '').replace('?', '')
             let key3 = element[0].toLowerCase()+'s'
-            if(!res[key][key2][key3].includes(element[1])){res[key][key2][key3].push(element[1])}
-            //console.log(res[key][key2][key3])
+            let eleminate = element.shift()
+            if(!res[key1][key2][key3].includes(element.join(','))){res[key1][key2][key3].push(element.join(','))}
+        }
+        else if (res[key][(element[1].replace(/\s/g, '_').replace('!', '').replace('?', '')).toLowerCase()] === undefined && index%2===0) {
+            let key2 = element[1].replace(/\s/g, '_').replace('!', '').replace('?', '')
+            let obj2 = {}
+            obj2[key.substring(0, key.length - 1)] = element[1]
+            let arr2 = arr[index+1].split(",")
+            let eleminate = arr2.shift()
+            obj2[arr[index+1].toLowerCase().split(",")[0]+'s'] = [arr2.join(',')]
+            res[key][key2.toLowerCase()] = obj2
+            // console.log(res[key][key2.toLowerCase()])
         }
     })
-    //console.log("gkhljl")
-    return res
-    
+    // console.log("================================")
+    // console.log(res)
+    return res 
 }
